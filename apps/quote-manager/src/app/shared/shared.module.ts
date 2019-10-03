62a45f2e14abc5a251c40761ab4e2f4f1dbb3103
Bartosz Jakubowski <bjakubov@gmail.com>
2019-10-03 18:52:54 +0200

    feat: Added router links to sidebar buttons

--- apps/quote-manager/src/app/shared/shared.module.ts
+++ apps/quote-manager/src/app/shared/shared.module.ts
@@ -2,20 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
 
 const DECLARATIONS = [
-  NavbarComponent,
-  SidebarComponent
+	NavbarComponent,
+	SidebarComponent
 ];
 
 const MODULES = [
-  CommonModule
+	CommonModule,
+	RouterModule
 ];
 
 @NgModule({
-  declarations: DECLARATIONS,
-  imports: MODULES,
-  exports: [...DECLARATIONS, ...MODULES]
+	declarations: DECLARATIONS,
+	imports: MODULES,
+	exports: [...DECLARATIONS, ...MODULES]
 
 })
 export class SharedModule { }
