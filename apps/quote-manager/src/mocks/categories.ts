302db0d4db84049c6324008fd7000991041b3aea
Bartosz Jakubowski <bjakubov@gmail.com>
2019-10-13 19:11:27 +0200

    feat: Added categories mock file

---
+++ apps/quote-manager/src/mocks/categories.ts
@@ -0,0 +1,32 @@
+import { QuoteCategoryInterface } from '../app/quotes/quote-category.interface';
+
+export const categoriesMock: QuoteCategoryInterface[] = [
+	{
+		id: '1',
+		name: 'motivation'
+	},
+	{
+		id: '2',
+		name: 'love'
+	},
+	{
+		id: '3',
+		name: 'ignorance'
+	},
+	{
+		id: '4',
+		name: 'time'
+	},
+	{
+		id: '5',
+		name: 'life'
+	},
+	{
+		id: '6',
+		name: 'leadership'
+	},
+	{
+		id: '7',
+		name: 'money'
+	},
+];
