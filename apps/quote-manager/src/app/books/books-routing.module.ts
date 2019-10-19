afc593d77348160fe9e411957c34d6bfde64a630
Bartosz Jakubowski <bjakubov@gmail.com>
2019-10-19 10:12:32 +0200

    refactor: Moved BooksQuotesComponent to book module

--- apps/quote-manager/src/app/books/books-routing.module.ts
+++ apps/quote-manager/src/app/books/books-routing.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BooksComponent } from './books.component';
+import { BooksListComponent } from './books-list/books-list.component';
+import { BooksQuotesComponent } from './books-quotes/books-quotes.component';
 
 const booksRoutes: Routes = [
 	{
 		path: 'books',
 		component: BooksComponent,
 		children: [
+			{
+				path: '',
+				redirectTo: 'quotes',
+				pathMatch: 'full'
+			},
+			{
+				path: 'quotes',
+				component: BooksQuotesComponent,
+			},
+			{
+				path: 'list',
+				component: BooksListComponent,
+			}
 		]
 	}
 ];
--- apps/quote-manager/src/app/books/books.module.ts
+++ apps/quote-manager/src/app/books/books.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BooksComponent } from './books.component';
 import { BooksRoutingModule } from './books-routing.module';
+import { BooksListComponent } from './books-list/books-list.component';
+import { BooksQuotesComponent } from './books-quotes/books-quotes.component';
 
 const DECLARATIONS = [
-	BooksComponent
+	BooksComponent,
+	BooksListComponent,
+	BooksQuotesComponent
 ];
 
 const MODULES = [
