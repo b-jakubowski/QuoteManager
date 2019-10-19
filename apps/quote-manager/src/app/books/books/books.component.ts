cef9b2d09b9def0ba4454b3a46b1058ecb8678b9
Bartosz Jakubowski <bjakubov@gmail.com>
2019-10-19 08:28:27 +0200

    feat: Added books component

---
+++ apps/quote-manager/src/app/books/books/books.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'quote-manager-books',
+  templateUrl: './books.component.html',
+  styleUrls: ['./books.component.css']
+})
+export class BooksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
