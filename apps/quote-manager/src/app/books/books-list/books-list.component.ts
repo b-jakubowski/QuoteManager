8e25efe15cff82f4592390a3d41b446748206f72
Bartosz Jakubowski <bjakubov@gmail.com>
2019-10-25 19:08:10 +0200

    refactor: Changed books-list css to scss

--- apps/quote-manager/src/app/books/books-list/books-list.component.ts
+++ apps/quote-manager/src/app/books/books-list/books-list.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
 	selector: 'quote-manager-books-list',
 	templateUrl: './books-list.component.html',
-	styleUrls: ['./books-list.component.css']
+	styleUrls: ['./books-list.component.scss']
 })
 export class BooksListComponent {
 
