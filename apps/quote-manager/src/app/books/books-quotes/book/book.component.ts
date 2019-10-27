7013762184202bd4402a06bf5fc27abaa2aff478
Bartosz Jakubowski <bjakubov@gmail.com>
2019-10-27 16:21:31 +0100

    fix: Change detection strategy for book component

--- apps/quote-manager/src/app/books/books-quotes/book/book.component.ts
+++ apps/quote-manager/src/app/books/books-quotes/book/book.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { BookInterface } from '../../book.interface';
 
 @Component({
 	selector: 'app-book',
 	templateUrl: './book.component.html',
-	styleUrls: ['./book.component.scss']
+	styleUrls: ['./book.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BookComponent {
 	@Input() book: BookInterface;
